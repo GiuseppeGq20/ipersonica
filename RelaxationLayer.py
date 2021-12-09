5f7381f2487ffb9545ee3348c676028c693a2d74
GiuseppeGq20 <giuseppe.giaquinto22@gmail.com>
2021-12-09 17:33:17 +0100

    mod relax layer.py

--- RelaxationLayer.py
+++ RelaxationLayer.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
 import flow.flows as fl
 # constants units: metric system
 
@@ -11,12 +12,12 @@ EPS2=0.213
 
 #cinetica chimica
 #eq Tv, vibrazione azoto
-C1=7.21e-4
+C1=7.21e-9
 C2=1.91e6
 TAU=5.68e-2
 
 #eq alpha, dissociazione ossigeno
-mO2=2,6566962e-26
+mO2=2.6566962e-26
 k=1.38e-23
 R=8.3144
 # ref quantity ,pag.55 cap 3
@@ -26,7 +27,7 @@ H_R=1.54e7
 S_R=260
 P_R=1.45e11
 V_R=1.06e-4
-
+KR=6.7e8
 T_VC=3364
 
 
@@ -40,9 +41,7 @@ def rhs(x,y,k1):
     dTvdx= (( np.exp(-(T_VC/T) + (T_VC/Tv)) - 1) / (1 - np.exp(-T_VC/T)) ) / (u*tv*TAU)
 
     # evolution of dissociation of O2
-    K= (4/ (mO2*V_R))* np.exp(- T_R/T)
-    
-    KR=A* np.exp(-E_A/(k*T))
+    K= (4/ (mO2*V_R) )* np.exp(- T_R/T)
 
     dalphadx= (2*(1+alpha+DELTA)*(rho**2)/(mO2**2))*KR*( ((1-alpha-BETA)/(4*rho))*K*mO2 -alpha**2 )/u
 
@@ -59,7 +58,8 @@ def rhs(x,y,k1):
 
 #TODO implement custom solver like in the book
 def solve():
-    while 
+    # while 
+    pass
 
 def main():
     Ma= 6.4
@@ -78,13 +78,23 @@ def main():
     air2=fl.normalShock(air)
     k1=air2.rho*u2
 
-    y0=[u2,air.T,0,air2.T,air2.T,air2.p]
+    y0=[u2,air.T,0,air2.T,air2.rho,air2.p]
 
     result=solve_ivp(
         lambda x,y: rhs(x,y,k1),
-
+       [0,1],
+       y0,
+       method="BDF" 
     )
-
+    return result
 
 if __name__=="__main__":
-    main()
+
+    result=main()
+    fig,ax=plt.subplots()
+    labels=("u","Tv","alpha","T","rho","p")
+    for y in result.y:
+        ax.loglog(result.t,y)
+    ax.grid()
+    ax.legend(labels)
+    plt.show()
