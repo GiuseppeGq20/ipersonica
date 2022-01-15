0884d42805bc1f972ed1145e711fe0097020b346
GiuseppeGq20 <giuseppe.giaquinto22@gmail.com>
2022-01-15 13:34:11 +0100

    mod shock relations

--- flows.py
+++ flows.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.optimize import root_scalar
-from dataclasses import dataclass, field
 
 # try do define an interface for Gas class
 
@@ -14,6 +13,11 @@ class Gas:
         self.cp=self.R* self.gamma /(self.gamma - 1)
         self.a= (self.gamma*self.R*self.T)**0.5 #m/s
         self.H= self.cp*self.T + 0.5*((self.Ma*self.a)**2)
+    
+    def update(self):
+        self.cp=self.R* self.gamma /(self.gamma - 1)
+        self.a= (self.gamma*self.R*self.T)**0.5 #m/s
+        self.H= self.cp*self.T + 0.5*((self.Ma*self.a)**2)
 
 def isentropicFlow():
     pass
@@ -24,16 +28,18 @@ def obliqueShock(theta: float,gas: Gas)-> float:
 
     Inputs:
     - theta: deviation angle of the flow in radians
-    - gas: Gas like dataclass
+    - gas: Gas like object
 
     Return:
     - beta: schock angle in radians
 
     Examples:
     shock of a 10 degree step
-    >>> air1=air(3)
-    >>> beta=obliqueShock(0.17,air1); round(beta,4)
-        0.4779
+    >>> dict_air={"Ma": 5,"gamma": 1.4,"R":287.05,"T":273.0,"rho":0.129,"p": 0.1*101325,"n" : 5}
+    >>> air1=Gas(dict_air)
+    >>> beta=obliqueShock(np.deg2rad(10),air1);
+    >>> np.isclose(beta, 0.3382)
+        True
 
     """
 
@@ -44,15 +50,17 @@ def obliqueShock(theta: float,gas: Gas)-> float:
     else : beta0 = theta*((gas.n+1)/(2*gas.n) + np.sqrt( ((gas.n+1)/(2*gas.n))**2 + (gas.Ma*theta)**-2 ))
 
     beta= root_scalar(
-        lambda beta: (1/(1+gas.n))*(1+ gas.n/((gas.Ma**2)*np.sin(beta)*np.sin(beta))) - np.tan(beta - theta)/np.tan(beta),
+        lambda beta: (1/(1+gas.n))*(1+ gas.n/((gas.Ma**2)*(np.sin(beta)**2))) - np.tan(beta - theta)/np.tan(beta),
         method='secant',
         x0= beta0,
         x1=1.3*beta0,
         maxiter=200,
-        xtol=1e-4
+        xtol=1e-6
     )
 
     if not beta.converged: raise RuntimeWarning(beta.flag)
+
+    if beta.root > np.deg2rad(90): raise RuntimeError("beta must be less than 90 degrees")
     
     return beta.root
 
@@ -80,23 +88,23 @@ def normalShock(gas: Gas,beta=np.pi/2):
 
 if __name__=="__main__":
 
-    # import doctest
-    # doctest.testmod()
-    Ma=np.linspace(1,20,50)
-    dict_air={
-    "Ma": 1,
-    "gamma": 1.4,
-    "R":287.05,
-    "T":273.0,
-    "rho":0.129,
-    "p": 0.1*101325, 
-    "n" : 5}
-    beta=np.zeros_like(Ma)
-    for i in range(Ma.size):
-        dict_air["Ma"]=Ma[i]
-        air=Gas(dict_air)
-        beta[i]=obliqueShock(np.deg2rad(5),air)
-    print(beta)
-    import matplotlib.pyplot as plt
-    plt.plot(beta)
-    plt.show()
+    import doctest
+    doctest.testmod()
+    # Ma=np.linspace(1.1,20,50)
+    # dict_air={
+    # "Ma": 1,
+    # "gamma": 1.4,
+    # "R":287.05,
+    # "T":273.0,
+    # "rho":0.129,
+    # "p": 0.1*101325, 
+    # "n" : 5}
+    # beta=np.zeros_like(Ma)
+    # for i in range(Ma.size):
+    #     dict_air["Ma"]=Ma[i]
+    #     air=Gas(dict_air)
+    #     beta[i]=obliqueShock(np.deg2rad(5),air)
+    # print(beta)
+    # import matplotlib.pyplot as plt
+    # plt.plot(np.rad2deg(beta))
+    # plt.show()
